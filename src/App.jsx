f0a8745d31295d3f0d3c0c87b830c2b77fac4b42
marazzax <77912698+marazzax@users.noreply.github.com>
2024-05-16 01:40:09 +0000

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Mohammed Abdul Razzak",
+  title: "Cloud Security Engineer",
+  email: "marazzax@gmail.com",
+  gitHub: "marazzax",
+  instagram: "marazzax",
+  linkedIn: "marazzax",
+  medium: "marazzax",
+  twitter: "marazzax",
+  youTube: "marazzax",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/desk.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "The MARAZZAX News",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "The MARAZZAX News is a Next.js app which shows the current news taken from MEDIASTACK and uses STEPZEN for GraphQL API.",
+    url: "https://news.marazzax.vercel.app/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Amazon Clone",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "An full fledge ecomm website.",
+    url: "http://amazon-clone-marazzax.vercel.app",
   },
   {
-    title: "My Resume Site",
+    title: "Web3 Amazon",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "A Web3 version of amazon.",
+    url: "https://github.com/marazzax/web3-amazon",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Travel India",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "An android app for travellers.",
+    url: "https://github.com/marazzax/TravelIndia",
   },
 ];
 
